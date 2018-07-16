e4c9a900750350b01e1c4f804e9a2abbac383b33
Seyran Haji <seyranhaji1997@gmail.com>
2018-07-16 18:45:38 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,13 @@
+function shout(string) {
+   return string.toUpperCase()
+}
+var uppercase = "HELLO!"
+ 
+uppercase.toUpperCase() === uppercase // true
+ 
+function whisper(string) {
+   return string.toLowerCase()
+}
+function sayHiToGrandma(string) {
+   return string.toUpperCase
+}
